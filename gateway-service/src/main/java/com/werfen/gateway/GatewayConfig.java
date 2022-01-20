f784cdeaa0658707e671fb495af584bee410ca64
alfonsserra <alfonsoserra@gmail.com>
2022-01-20 09:17:37 +0100

    Supports compose and k8s at the same time.

--- gateway-service/src/main/java/com/werfen/gateway/GatewayConfig.java
+++ gateway-service/src/main/java/com/werfen/gateway/GatewayConfig.java
@@ -2,6 +2,9 @@ package com.werfen.gateway;
 
 import com.werfen.gateway.tenant.TenantDTO;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
@@ -12,37 +15,48 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
 @Component
 public class GatewayConfig {
+
+    private Log logger = LogFactory.getLog(this.getClass());
 
     private static final String TENANT_HEADER = "X-TenantID";
 
     private final WebClient.Builder client;
 
+    @Value("${app.k8s:true}")
+    private boolean k8s;
+
     @Bean
     RouteLocator gateway(RouteLocatorBuilder rlb) {
         return rlb.routes()
+                .route(routeSpec -> routeSpec.path("/swagger")
+                        .uri(getURIForLaboratoryService()+"/swagger-ui.html"))
                 .route(routeSpec -> routeSpec.path("/inventory/**")
                         .filters(spec -> removePathPrefix(spec, "/inventory").filters(this::changeTenantFromIdToSchema))
-                        .uri("lb://inventory-service"))
+                        .uri(getURIForInventoryService()))
                 .route(routeSpec -> routeSpec.path("/laboratory/**")
                         .filters(spec -> removePathPrefix(spec, "/laboratory").filters(this::changeTenantFromIdToSchema))
-                        .uri("lb://laboratory-service"))
+                        .uri(getURIForLaboratoryService()))
                 .route(routeSpec -> routeSpec.path("/api/**")
                         .filters(f -> f.filter(this::changeTenantFromIdToSchema))
-                        .uri("lb://laboratory-service"))
+                        .uri(getURIForLaboratoryService()))
                 .route(routeSpec -> routeSpec.path("/**")
-                        .uri("lb://laboratory-service"))
+                        .uri(getURIForLaboratoryService()))
                 .build();
     }
 
     private GatewayFilterSpec removePathPrefix(GatewayFilterSpec spec, String prefix) {
+        logger.info("Removing prefix: "+prefix);
         return spec.rewritePath(prefix, "");
     }
 
     private Mono<Void> changeTenantFromIdToSchema(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return client.baseUrl("lb://tenant-service").build().get()
+        logger.info("Getting the tenant schema for tenant id: "+ exchange.getRequest().getHeaders().getFirst(TENANT_HEADER));
+        return client.baseUrl(getURIForTenantService()).build().get()
                 .uri("/tenants/" + "3fa85f64-5717-4562-b3fc-2c963f66afa6")
                 .retrieve()
                 .bodyToMono(TenantDTO.class).map(TenantDTO::getSchema)
@@ -56,7 +70,7 @@ public class GatewayConfig {
     }
 
     private Mono<Void> changeTenantFromIdToSchemaGood(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return client.baseUrl("lb://tenant-service").build().get()
+        return client.baseUrl(getURIForTenantService()).build().get()
                 .uri("/tenants/" + exchange.getRequest().getHeaders().getFirst(TENANT_HEADER))
                 .retrieve()
                 .bodyToMono(TenantDTO.class).map(TenantDTO::getSchema)
@@ -65,5 +79,29 @@ public class GatewayConfig {
                     return exchange;
                 })
                 .flatMap(chain::filter);
+    }
+
+    private String getURIForInventoryService() {
+        if (k8s) {
+            return "http://inventory-service.default.svc.cluster.local:8885";
+        } else {
+            return "lb://inventory-service";
+        }
+    }
+
+    private String getURIForLaboratoryService() {
+        if (k8s) {
+            return "http://laboratory-service.default.svc.cluster.local:8886";
+        } else {
+            return "lb://laboratory-service";
+        }
+    }
+
+    private String getURIForTenantService() {
+        if (k8s) {
+            return "http://tenant-service.default.svc.cluster.local:8887";
+        } else {
+            return "lb://tenant-service";
+        }
     }
 }
